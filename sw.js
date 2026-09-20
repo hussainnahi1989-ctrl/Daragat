d978c85932a9f2eaa0383a5a56c1fbf03d253c6c
hussainnahi1989-ctrl <hussain.nahi1989@gmail.com>
2026-09-20 11:23:25 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "daragat-v1";
+const CACHE = "daragat-v1.4.2";
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./admin.html", "./manifest.json", "./icon-192.png", "./icon-512.png"])));
   self.skipWaiting();
