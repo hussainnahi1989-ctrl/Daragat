6765fdd736ca78ceb7b7cae78ad4d98db0f13179
hussainnahi1989-ctrl <hussain.nahi1989@gmail.com>
2026-09-23 18:18:35 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "daragat-v1.10.5";
+const CACHE = "daragat-v1.10.6";
 const CORE = ["./", "./index.html", "./admin.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   // Fetch fresh copies (bypass the browser HTTP cache) so a new version never installs stale files.
